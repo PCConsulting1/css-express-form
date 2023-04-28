5cd7170f854fdde5302826d8496b2d31bb7e442f
Gerald <codyhenry527@gmail.com>
2023-04-28 14:48:58 -0400

    added services to home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,13 @@
 import WelcomeBanner from '../components/Welcome';
 import PriceList from '../components/prices/PriceList';
+import ServiceList from '../components/services/ServiceList';
 
 export default function () {
   return (
     <>
       <WelcomeBanner />
       <PriceList />
+      <ServiceList />
     </>
   );
 }
