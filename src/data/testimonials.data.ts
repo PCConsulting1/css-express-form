9d82af460e22a8a59f1fa89432fd9eab7c1ade2e
Gerald <codyhenry527@gmail.com>
2023-04-28 13:49:18 -0400

    added icons and data

---
+++ src/data/testimonials.data.ts
@@ -0,0 +1,7 @@
+const testimonials = [
+    { message: 'Your company really helped me take the fear and anxiety over the answering those complicated questions the CSS Profile asks of us.', state: 'FL' },
+    { message: 'It was a great help to have one of your team members fill out the 3+ hour form for us', state: 'MD' },
+    { message: 'I didn’t know where to even begin to start answering the difficult CSS Profile questions and what the correct information was for the boxes. Thank you so much for doing this application for us', state: 'CA'}
+]
+  
+  export default testimonials
